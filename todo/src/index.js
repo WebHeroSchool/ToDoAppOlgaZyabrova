3504c1a288dd27e819c4345f272f9eb95051e755
ZyabrovaOlga <ingasholga@gmail.com>
2020-11-10 21:55:44 +0700

    react-components

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {unit} from './App'
+import App from './App'
 
-ReactDOM.render(unit, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
